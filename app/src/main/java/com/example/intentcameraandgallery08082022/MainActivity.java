c0a131cd14b15125a7fa617332c3cc432d6b9eac
Phat <phatdroid94@gmail.com>
2022-10-21 20:42:20 +0700

    xu ly request

--- app/src/main/java/com/example/intentcameraandgallery08082022/MainActivity.java
+++ app/src/main/java/com/example/intentcameraandgallery08082022/MainActivity.java
@@ -11,8 +11,10 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -37,10 +39,18 @@ public class MainActivity extends AppCompatActivity {
                 if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
                         != PackageManager.PERMISSION_GRANTED
                 ) {
-                    requestPermissions(
-                            new String[]{Manifest.permission.CAMERA},
-                            REQUEST_CODE_CAMERA
-                    );
+                    if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                        requestPermissions(
+                                new String[]{Manifest.permission.CAMERA},
+                                REQUEST_CODE_CAMERA
+                        );
+                    } else {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                        intent.setData(uri);
+                        startActivity(intent);
+                    }
+
                 } else {
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     cameraLauncher.launch(intent);
